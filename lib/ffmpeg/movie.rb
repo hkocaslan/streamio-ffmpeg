95b4d40e5cc89df48aaeb4c83e6e633302e9d819
hatice koc <hatice@hatices-MacBook-Pro.local>
2016-03-26 16:44:23 -0700

    Remove file existance check for remote files

--- lib/ffmpeg/movie.rb
+++ lib/ffmpeg/movie.rb
@@ -9,7 +9,9 @@ module FFMPEG
     attr_reader :container
 
     def initialize(path)
-      raise Errno::ENOENT, "the file '#{path}' does not exist" unless File.exist?(path)
+      if(!path.includes("http://"))
+        raise Errno::ENOENT, "the file '#{path}' does not exist" unless File.exist?(path)
+      end
 
       @path = path
 
