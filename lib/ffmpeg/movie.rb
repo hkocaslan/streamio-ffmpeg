4f51ac83e36fc4f6b9f4a2bdd747e55a19493b0e
hatice koc <hatice@hatices-MacBook-Pro.local>
2016-03-26 16:45:52 -0700

    Command fix

--- lib/ffmpeg/movie.rb
+++ lib/ffmpeg/movie.rb
@@ -9,7 +9,7 @@ module FFMPEG
     attr_reader :container
 
     def initialize(path)
-      if(!path.includes("http://"))
+      if(!path.include?("http://"))
         raise Errno::ENOENT, "the file '#{path}' does not exist" unless File.exist?(path)
       end
 
